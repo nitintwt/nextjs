c7194e683a1df133e86be459753ce6e0cc94b4c1
nitintwt <nitintwt27@gmail.com>
2024-02-05 11:18:53 +0530

    layout and Home page setup

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css'
+
+
+export const metadata= {
+  title: "promtopia",
+  description:"Discover & share AI prompts"
+}
+
+function RootLayout({children}) {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient'/>
+        </div>
+
+        <main className='app'>
+          {children}
+        </main>
+      </body>
+    </html>
+    
+  )
+}
+
+export default RootLayout
